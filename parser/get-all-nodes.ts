22ae45380ae55a309570403b7ccc46444286234b
Alexandro <arvitaly@list.ru>
2017-09-18 18:16:47 +0300

    fix types

--- parser/get-all-nodes.ts
+++ parser/get-all-nodes.ts
@@ -2,7 +2,10 @@ import GetNodeList from "./node-list";
 import GetNode from "./node-page";
 export const GetAllNodes = ({ getNode = GetNode, getNodeList = GetNodeList }) => async () => {
     const nodes = await getNodeList();
-    return await Promise.all(nodes.map(async (n) => ({ ...n, data: await getNode(n.name) })));
+    return await Promise.all(nodes.map(async (n) => {
+        const data = { ...n, data: await getNode(n.name) };
+        return data;
+    }));
 };
 
 export default GetAllNodes({});
